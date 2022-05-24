92aa43d83b8655e92d9009c57dabe5338f25d034
jonataspinto <jonatastdb_jp@hotmail.com>
2022-05-24 12:08:33 -0300

    chore: adding return to methods

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const BASE_API_NATIONAL_HOLIDAYS = "https://brasilapi.com.br/api/feriados/v1/";
 */
 
 function getNationalHolidays(year) {
+  return year
   // implemente aqui
 }
 
@@ -37,6 +38,7 @@ function getNationalHolidays(year) {
 */
 function getCarnivalDatesFrom2020To2030() {
   // implemente aqui
+  return null
 }
 
 module.exports = {
